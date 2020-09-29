0931c6f3dd9114e2fc8360ace0f7d11bdc993ffa
vcaruso <vinnycar0923@gmail.com>
2020-09-28 17:35:45 -0700

    enabled idb

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -19,7 +19,7 @@ request.onsuccess = function (event) {
     // check if app is online, if yes run uploadPizza() function to send all local db data to api
     if (navigator.onLine) {
         // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadPizza();
+        uploadPizza();
     }
 };
 
